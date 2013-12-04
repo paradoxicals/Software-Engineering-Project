77acb96fd51d97235b272fe0f0c6243936510bbf
serkan <serkan.danisan@gmail.com>
2013-12-04 09:39:41 +0200

    Kullanıcı Kayıt İşlemi Php Dosyası

---
+++ php/KULLANICIKAYIT.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>
+     Anket Sistemi Kullanıcı Kayıt Ekranı
+    </title>   
+	<link rel="shortcut icon" href="" /> 
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     <script type="text/javascript" src="MST/Kutuphaneler/ext-3.4.0/adapter/ext/ext-base.js"></script>
+   <script type="text/javascript" src="MST/Kutuphaneler/ext-3.4.0/ext-all.js"></script> 
+    <link rel="stylesheet" type="text/css" href="MST/Kutuphaneler/ext-3.4.0/resources/css/ext-all.css"> 
+    
+       <link rel="stylesheet" type="text/css" href="MST/Kutuphaneler/ext-3.4.0/resources/css/xtheme-access.css"/>
+   <script src="MST/Kutuphaneler/kullanicilar_public.js"></script>
+   <script src="MST/Kutuphaneler/ext-3.4.0/examples/ux/Spinner.js"></script>
+   <script src="MST/Kutuphaneler/ext-3.4.0/examples/ux/SpinnerField.js"></script>
+     <link rel="stylesheet" type="text/css" href="MST/Kutuphaneler/ext-3.4.0/examples/ux/css/Spinner.css">
+ 
+ 
+</head>
+<body bgcolor="dfe8f6">
+
+</body>
+</html>
