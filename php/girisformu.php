4db5c6ca0cada9a8152a8c06bfb05ead14044dc6
mustafaozsay <mustafa.ozsay@gmail.com>
2013-12-06 11:04:45 +0200

    Giriş Formu

---
+++ php/girisformu.php
@@ -0,0 +1,35 @@
+
+
+<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
+<? ob_start();     ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Anket Sistemi Giriş Formu</title>
+</head>
+
+<body>
+<p>&nbsp;</p>
+ 
+<form method="post" action="giris.php">
+Kullanıcı Adı : <br> 
+<label for="kullaniciAdi"></label>
+<input type="text" name="kullaniciAdi" id="kullaniciAdi">
+ <br>
+Şifre :<br> 
+<label for="sifre"></label>
+<input type="password" name="sifre" id="sifre">
+ <br>
+<br>
+<input type="submit" value="Giris Yap">
+</form>
+</div>
+</div>
+<p>&nbsp;</p>
+<script type="text/javascript">
+ 
+</script>
+</body>
+</html>
