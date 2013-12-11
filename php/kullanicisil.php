f371713d8ca64ff6ba33dd75c702ea174005dc06
mustafaozsay <mustafa.ozsay@gmail.com>
2013-12-11 21:52:28 +0200

    Kullanıcı Silme

---
+++ php/kullanicisil.php
@@ -0,0 +1,7 @@
+﻿<?php     
+$gelenID=$_GET['id']; 
+$db_handle =  "host=localhost port=5432 dbname=postgis20 user=postgres password=qaqaqa"; 
+$dbconn = pg_connect($db_handle);   
+$sorgu= "delete from uyetemelbilgileri where id='$gelenID'";  
+pg_query($dbconn, $sorgu);   
+?>
