b1892b850589526b15efbe5562cb91a92cbdb18f
mustafatugrul <boztepeee@gmail.com>
2013-12-15 19:31:16 +0200

    numeric filter devamı

--- Kütüphaneler/NumericFilter.js
+++ Kütüphaneler/NumericFilter.js
@@ -12,7 +12,7 @@ Ext.ux.grid.filter.NumericFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
 
  
     menuItemCfgs : {
-        emptyText: 'Filtrelenecek Değer...',
+        emptyText: 'Filtrelenecek De?er...',
         selectOnFocus: true,
         width: 125
     },
@@ -39,5 +39,58 @@ Ext.ux.grid.filter.NumericFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
         // relay the event fired by the menu
         this.menu.on('update', this.fireUpdate, this);
     },
+    
+    /**
+     * @private
+     * Template method that is to get and return the value of the filter.
+     * @return {String} The value of this filter
+     */
+    getValue : function () {
+        return this.menu.getValue();
+    },
 
+    setValue : function (value) {
+        this.menu.setValue(value);
+    },
+
+    
+    isActivatable : function () {
+        var values = this.getValue();
+        for (key in values) {
+            if (values[key] !== undefined) {
+                return true;
+            }
+        }
+        return false;
+    },
+   
+    getSerialArgs : function () {
+        var key,
+            args = [],
+            values = this.menu.getValue();
+        for (key in values) {
+            args.push({
+                type: 'numeric',
+                comparison: key,
+                value: values[key]
+            });
+        }
+        return args;
+    },
+
+   
+    validateRecord : function (record) {
+        var val = record.get(this.dataIndex),
+            values = this.getValue();
+        if (values.eq !== undefined && val != values.eq) {
+            return false;
+        }
+        if (values.lt !== undefined && val >= values.lt) {
+            return false;
+        }
+        if (values.gt !== undefined && val <= values.gt) {
+            return false;
+        }
+        return true;
+    }
 });
