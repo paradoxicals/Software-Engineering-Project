8d1d51e2cc49b517bcbac6e93ec703b3bde336b4
serkan <serkan.danisan@gmail.com>
2013-12-20 11:39:25 +0200

    Araç Çubuğu Oluşturulması

---
+++ araccubugu.js
@@ -0,0 +1,72 @@
+	function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+	
+	var yetki_duzeyi = readCookie('yetki');
+	var giris_yapan_ad=readCookie('ad');
+	var giris_yapan_soyad=readCookie('soyad');
+	
+	
+	var arac_cubugu;
+	
+	if(yetki_duzeyi==1){
+	  arac_cubugu= new Ext.Toolbar({
+         style:'margin-top: 0px',//100
+		 height:30,
+	     region: "north",      
+	    items:[
+		{ 
+                text:'Sorgulama İşlemleri',
+                xtype:'splitbutton',
+                icon:'Images/16/moduller.png',
+                menu:[
+				{
+                        text:'Şehir İşlemleri',
+						 handler:function () {
+						sehirWindow.show();
+                        }
+						},
+					
+                  {
+                        text:'Bölge İşlemleri',
+						 handler:function () {
+						bolgeWindow.show();
+                        }
+						
+				} ,				 
+					
+            	{
+                    text:'Seçmen İşlemleri',
+                      
+                        handler:function () {
+							secmenWindow.show();  
+						    
+                        }
+                },
+					
+						{
+                        text:'Anket İşlemleri',
+                        icon:'Images/16/maden.png',
+                        handler:function () {
+						anketWindow.show();
+                        }
+                    } 
+            ]
+            },
+            
+		
+	   
+               
+				  
+					
+		 
+          
+		  
+      
