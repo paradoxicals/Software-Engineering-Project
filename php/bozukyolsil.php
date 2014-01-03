1538e60a79bfb835ad7fa3b19a1f6d1c3efc6517
mustafaozsay <mustafa.ozsay@gmail.com>
2014-01-03 23:51:42 +0200

    bozukYolSil

---
+++ php/bozukyolsil.php
@@ -0,0 +1,7 @@
+﻿<?php     
+$gelenGID=$_GET['gid'];  
+$db_handle =  "host=localhost port=5432 dbname=postgis20 user=postgres password=qaqaqa"; 
+$dbconn = pg_connect($db_handle);    
+$sorgu= "delete from bozukyol_900913 where gid='$gelenGID'";  
+pg_query($dbconn, $sorgu); 
+?>
