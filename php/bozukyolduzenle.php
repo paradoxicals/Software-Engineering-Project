aa14d9260960d890ff5c5466240a9f84608885ed
mustafaozsay <mustafa.ozsay@gmail.com>
2014-01-03 23:53:40 +0200

    YolDuzenle

---
+++ php/bozukyolduzenle.php
@@ -0,0 +1,17 @@
+﻿<?php   
+
+$db_handle =  "host=localhost port=5432 dbname=postgis20 user=postgres password=qaqaqa"; 
+$dbconn = pg_connect($db_handle); 
+ 
+   $gelenKayitID=$_POST['id'];
+   $gelenBaslangicKm=$_POST['baslangic'];
+   $gelenBitisKm=$_POST['bitis'];
+   $gelenYolId=$_POST['yolid'];  
+   $gelenyapimyili=$_POST['yil']; 
+   
+  
+    $query = "UPDATE bozukyol_900913 SET anaobjeid = '$gelenYolId', kmbaslangic = '$gelenBaslangicKm', kmbitis = '$gelenBitisKm', yapimyili = '$gelenyapimyili'  WHERE gid=$gelenKayitID";
+    $result = pg_query($dbconn,$query);
+    echo '1';        // success 
+ 
+?>
