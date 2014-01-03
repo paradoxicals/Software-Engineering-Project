5e48b6ea65a41c6bbf7e7ef832efb42b102cce01
mustafaozsay <mustafa.ozsay@gmail.com>
2014-01-03 23:52:16 +0200

    YerleşimDüzenle

---
+++ php/koyyerlesimduzenle.php
@@ -0,0 +1,15 @@
+﻿<?php   
+
+$db_handle =  "host=localhost port=5432 dbname=postgis20 user=postgres password=qaqaqa"; 
+$dbconn = pg_connect($db_handle); 
+ 
+   $gelenKayitID=$_POST['id'];
+   
+   $gelenObjectID=$_POST['objectid'];
+   $gelenAdi=$_POST['adi'];  
+	
+    $query = "UPDATE koy_yerlesim_nonzm SET objectid ='$gelenObjectID', adi ='$gelenAdi' WHERE gid='$gelenKayitID'";
+    $result = pg_query($dbconn,$query);
+    echo '1';        // success 
+ 
+?>
