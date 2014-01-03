210f67a308254269144e39f4c3a9fe749ea9e73b
mustafaozsay <mustafa.ozsay@gmail.com>
2014-01-03 23:51:02 +0200

    egimYolSil

---
+++ php/egimyolsil.php
@@ -0,0 +1,7 @@
+﻿<?php     
+$gelenGID=$_GET['gid'];  
+$db_handle =  "host=localhost port=5432 dbname=postgis20 user=postgres password=qaqaqa"; 
+$dbconn = pg_connect($db_handle);    
+$sorgu= "delete from egimyol_900913 where gid='$gelenGID'";  
+pg_query($dbconn, $sorgu); 
+?>
